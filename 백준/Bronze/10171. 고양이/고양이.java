30a83b954842819da557503a7560e9b1e57d9c34
lsy000428 <52476199+lsy000428@users.noreply.github.com>
2022-11-15 17:44:26 +0900

    [Bronze V] Title: 고양이, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		java.util.Scanner scanner = new Scanner(System.in);
+		
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+
+}
