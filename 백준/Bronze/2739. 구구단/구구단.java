b1a2e184d87b6f46405eb6251687d1e53cdf2974
lsy000428 <52476199+lsy000428@users.noreply.github.com>
2022-11-17 18:28:50 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+	java.util.Scanner sc = new Scanner(System.in);
+	
+	int a = sc.nextInt();
+	//int b = sc.nextInt();
+	int c = 1;
+	for(c=1;c<10;c++) {
+		System.out.println(a+" * "+c+" = "+a*c);
+	}
+}
+}	
